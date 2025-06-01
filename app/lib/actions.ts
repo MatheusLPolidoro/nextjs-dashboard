439a7dc8eef328e5983dd2951fe54fa1a37251cd
MatheusLPolidoro <mattpolidoro4@gmail.com>
2025-06-01 11:44:25 -0300

    add auth

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,5 +1,7 @@
 'use server';
 
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 import { z } from 'zod';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
@@ -112,4 +114,24 @@ export async function deleteInvoice(id: string) {
         console.log(error);
     }
     revalidatePath('/dashboard/invoices');
+}
+
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
 }
